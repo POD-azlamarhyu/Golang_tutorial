45e9fbdbf674d2f0b827ff35173d37c0b4667a57
dev-anego <adrague.ggoch@gmail.com>
2026-06-06 16:03:45 +0900

    add typeassertion

---
+++ src/struct/typeassertion.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"time"
+	"bufio"
+	"strings"
+	"strconv"
+	"log"
+)
+
+func hogeHoge(){
+		// Get current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current working directory:", err)
+		return
+	}
+	fmt.Printf("Current working directory: %s\n", cwd)
+
+	// List files in current directory
+	files, err := os.ReadDir(cwd)
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+	fmt.Println("Files in current directory:")
+	for _, file := range files {
+		fmt.Println(" -", file.Name())
+	}
+
+	// Create a new file and write to it
+	newFilePath := filepath.Join(cwd, "example.txt")
+	newFile, err := os.Create(newFilePath)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer newFile.Close()
+
+	_, err = newFile.WriteString("This is an example file created by Go.\n")
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	fmt.Printf("File '%s' created and written to successfully.\n", newFilePath)
+
+	// Read the contents of the file
+	readFile, err := os.Open(newFilePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer readFile.Close()
+
+	scanner := bufio.NewScanner(readFile)
+	fmt.Printf("Contents of '%s':\n", newFilePath)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// Example of string manipulation
+	sampleString := "Go is a great programming language!"
+	words := strings.Fields(sampleString)
+	fmt.Printf("Words in sample string: %v\n", words)
+
+	// Example of converting string to integer
+	numStr := "42"
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		fmt.Println("Error converting string to integer:", err)
+		return
+	}
+	fmt.Printf("Converted number: %d\n", num)
+
+	log.Println("This is a log message.")
+}
+
+func hoge(i interface{}) {
+	switch v := i.(type) {
+	case string:
+		v += " is a string"
+		fmt.Printf("The string is: %s\n", v)
+	case int:
+		v *= 1000
+		fmt.Printf("The integer is: %d\n", v)
+	default:
+		fmt.Printf("Unknown type: %T\n", v)
+	}
+}
+
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	fmt.Printf("Go version: %s\n", runtime.Version())
+	fmt.Println("OS: ",runtime.GOOS)
+	fmt.Printf("Current time: %s\n", time.Now().Format(time.RFC1123))
+	fmt.Println()
+
+	hoge(420)
+	hoge("Go")
+	hoge(3.14)
+}
