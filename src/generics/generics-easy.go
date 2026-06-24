cb0904b564477d17bd6bcdb5017b30b339855254
dev-anego <adrague.ggoch@gmail.com>
2026-06-24 21:36:26 +0900

    add easy example for generics

---
+++ src/generics/generics-easy.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func hogeHoge() {
+	// Get current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current working directory:", err)
+		return
+	}
+	fmt.Printf("Current working directory: %s\n", cwd)
+
+	// List files in current directory
+	files, err := os.ReadDir(cwd)
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+	fmt.Println("Files in current directory:")
+	for _, file := range files {
+		fmt.Println(" -", file.Name())
+	}
+
+	// Create a new file and write to it
+	newFilePath := filepath.Join(cwd, "example.txt")
+	newFile, err := os.Create(newFilePath)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer newFile.Close()
+
+	_, err = newFile.WriteString("This is an example file created by Go.\n")
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	fmt.Printf("File '%s' created and written to successfully.\n", newFilePath)
+
+	// Read the contents of the file
+	readFile, err := os.Open(newFilePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer readFile.Close()
+
+	scanner := bufio.NewScanner(readFile)
+	fmt.Printf("Contents of '%s':\n", newFilePath)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// Example of string manipulation
+	sampleString := "Go is a great programming language!"
+	words := strings.Fields(sampleString)
+	fmt.Printf("Words in sample string: %v\n", words)
+
+	// Example of converting string to integer
+	numStr := "42"
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		fmt.Println("Error converting string to integer:", err)
+		return
+	}
+	fmt.Printf("Converted number: %d\n", num)
+
+	log.Println("This is a log message.")
+}
+
+/** 
+* * Generics 簡単な例
+* ? https://zenn.dev/exmedia/articles/go-generics-for-beginner
+*/
+func function[T int | float32 | float64](param[] T) T{
+	var result T
+	for _, v := range param {
+		result += v
+	}
+	return result
+}
+
+
+// Generics 例
+func main() {
+	startTime := time.Now()
+	fmt.Println("Hello, World!")
+
+	fmt.Printf("Go version: %s\n", runtime.Version())
+	fmt.Println("OS: ", runtime.GOOS)
+	fmt.Printf("Current time: %s\n", time.Now().Format(time.RFC1123))
+	fmt.Println()
+
+	intNumbers := []int{1, 2, 3, 4, 5}
+	floatNumbers := []float64{1.5, 2.5, 3.5, 4.5, 5.5}
+	sumInt := function(intNumbers)
+	fmt.Printf("Sum of numbers: %d\n", sumInt)
+
+	sumFloat := function(floatNumbers)
+	fmt.Printf("Sum of float numbers: %f\n", sumFloat)
+
+	fmt.Println("####################################################################################")
+	elapsedTime := time.Since(startTime)
+	fmt.Printf("Execution time: %s\n", elapsedTime)
+}
